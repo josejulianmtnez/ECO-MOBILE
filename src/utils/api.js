07b3e989ce64de6f2b85d140a65a42e5175664c7
josejulianmtnez <martinez.delacruzjosejulian32@gmail.com>
2025-10-11 11:53:19 -0600

    REFACTOR: rename endpoints

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const API_URL = "http://192.168.0.16:3500";
 export const signup = async (name, email, role, linked_child, password) => {
-  const res = await fetch(`${API_URL}/api/auth/sign_up`, {
+  const res = await fetch(`${API_URL}/api/auth/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, email, role, linked_child, password }),
@@ -9,7 +9,7 @@ export const signup = async (name, email, role, linked_child, password) => {
 };
 
 export const login = async (email, password) => {
-  const res = await fetch(`${API_URL}/api/auth/log_in`, {
+  const res = await fetch(`${API_URL}/api/auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
